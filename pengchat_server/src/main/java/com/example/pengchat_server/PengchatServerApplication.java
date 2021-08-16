920d99308b36cef0139be353f25432f76b9ccffd
Angol <Angol.Near.Solution@gmail.com>
2021-08-16 09:06:32 -0700

    first commit

---
+++ pengchat_server/src/main/java/com/example/pengchat_server/PengchatServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pengchat_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PengchatServerApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PengchatServerApplication.class, args);
+	}
+
+}
---
+++ pengchat_server/src/test/java/com/example/pengchat_server/PengchatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pengchat_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PengchatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
