a9f815cecefacd71f62b504ca681b75eadc3e32d
Jang-Angol <Angol.Near.Solution@gmail.com>
2021-10-27 00:02:54 +0900

    modify redux

--- pengchat_client/src/modules/loading.js
+++ pengchat_client/src/modules/loading.js
@@ -18,16 +18,18 @@ export const finishLoading = createAction(
 
 const initialState = {};
 
-const loading = handleActions({
-  [START_LOADING]: (state, action) => ({
-    ...state,
-    [action.payload]: true,
-  }),
-  [FINISH_LOADING]: (state, action) => ({
-    ...state,
-    [action.payload]: true,
-  }),
-  initialState,
-});
+const loading = handleActions(
+  {
+    [START_LOADING]: (state, action) => ({
+      ...state,
+      [action.payload]: true,
+    }),
+    [FINISH_LOADING]: (state, action) => ({
+      ...state,
+      [action.payload]: false,
+    }),
+  },
+  initialState
+);
 
 export default loading;
