67ca4dc7b769a870790bdffb5d698e6c22e4f93b
Jang-Angol <Angol.Near.Solution@gmail.com>
2021-10-26 23:24:47 +0900

    loading modify

--- pengchat_client/src/modules/loading.js
+++ pengchat_client/src/modules/loading.js
@@ -6,26 +6,28 @@ const FINISH_LOADING = "loading/FINISH_LOADING";
 /*
  요청을 위한 액션 타입을 payload로 설정 ex) "sample/GET_POST"
 */
+export const startLoading = createAction(
+  START_LOADING,
+  (requestType) => requestType
+);
 
 export const finishLoading = createAction(
-    FINISH_LOADING,
-    requestType => requestType
+  FINISH_LOADING,
+  (requestType) => requestType
 );
 
 const initialState = {};
 
-const loading = handleAction(
-    {
-        [START_LOADING]: (state,action) => ({
-            ...state,
-            [action.payload]: true,
-        }),
-        [FINISH_LOADING]: (state,action) => ({
-            ...state,
-            [action.payload]: true,
-        }),
-        initialState
-    }
-);
+const loading = handleAction({
+  [START_LOADING]: (state, action) => ({
+    ...state,
+    [action.payload]: true,
+  }),
+  [FINISH_LOADING]: (state, action) => ({
+    ...state,
+    [action.payload]: true,
+  }),
+  initialState,
+});
 
 export default loading;
