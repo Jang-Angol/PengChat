e4ed934e1165049422cc990543f2b71d3fba4265
Jang-Angol <Angol.Near.Solution@gmail.com>
2021-10-26 23:21:14 +0900

    modify saga

--- pengchat_client/src/lib/createRequestSaga.js
+++ pengchat_client/src/lib/createRequestSaga.js
@@ -1,5 +1,4 @@
 import { call, put } from "redux-saga/effects";
-import { startLoading, finishLoading } from "../modules/loading";
 
 export const createRequestActionTypes = (type) => {
   const SUCCESS = `${type}_SUCCESS`;
