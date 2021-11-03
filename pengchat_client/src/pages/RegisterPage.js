bc22078a5e8a9d916c8373de396075950e45f06d
Jang-Angol <Angol.Near.Solution@gmail.com>
2021-11-04 00:28:12 +0900

    register error handling

--- pengchat_client/src/pages/RegisterPage.js
+++ pengchat_client/src/pages/RegisterPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Container } from "@material-ui/core";
 import { Link } from "react-router-dom";
